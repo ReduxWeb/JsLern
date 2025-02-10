db19fda6456a3bb2c667bfc69aacbdcca3d86432
Artur <140401648+ReduxWeb@users.noreply.github.com>
2025-02-10 17:04:28 +0300

    Create base.js

---
+++ base.js
@@ -0,0 +1,11 @@
+//Задание №1
+const a = 10;
+const b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+//Задание №2
+const name = prompt("Как вас зовут ?");
+console.log(`Привет, ${name}`);
