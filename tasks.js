155c49ffb00fa4b46287b85190d3f99dd2e699f7
Artur <140401648+ReduxWeb@users.noreply.github.com>
2025-02-18 17:03:52 +0300

    Add files via upload

---
+++ tasks.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Задание № 1
+function isEvent(number) {
+  if (number % 2 == 0) {
+    return "Четное число!";
+  }
+  return "Не четное число!";
+}
+console.log(isEvent(11));
+
+// Задание № 2
+const user = {
+  name: "Artur",
+  age: 26,
+  email: "seme.98@bk.ru",
+};
+console.log(user);
